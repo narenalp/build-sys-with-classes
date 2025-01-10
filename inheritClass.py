3201c070aafc8aeb96490eb9c1f18d8b80f64377
narenalp <naren.alp@gmail.com>
2025-01-10 21:21:07 +0530

    Labrador inherit Doggo

--- inheritClass.py
+++ inheritClass.py
@@ -5,7 +5,36 @@ class Doggo:
         self.age = age
 
     def __str__(self):
-        return f"{self.name} is {self.age} years old"
+        return f"{self.name} is {self.age} years old."
     
     def speak(self, sound):
         return f"{self.name} says {sound}"
+    
+miles = Doggo("Miles", 4)
+buddy = Doggo("Buddy", 9)
+jack = Doggo("Jack", 3)
+jim = Doggo("Jim", 5)
+
+# create inherited class  and its instance 
+
+class Labrador(Doggo):
+    pass
+
+gandhi = Labrador("Gandhi", 13)
+
+print(gandhi.name, gandhi.age)
+print(gandhi.species)
+print(gandhi)
+gandhi.speak("buf")
+print(isinstance(gandhi, Labrador))
+
+print(isinstance(gandhi, Doggo))
+
+# Extending a Parent CLass
+class JackRusselTerrier(Doggo):
+    def speak(self, sound="Arf"):
+        return f"{self.name} says {sound}"
+    
+miles = JackRusselTerrier("Miles", 4)
+miles.speak()
+miles.speak("Grr")
