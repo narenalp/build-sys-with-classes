cf04c2eaf5881f6421c75c9ac296eda745fef650
narenalp <naren.alp@gmail.com>
2025-01-10 21:00:33 +0530

    Labrador inherit Doggod

--- inheritClass.py
+++ inheritClass.py
@@ -10,7 +10,22 @@ class Doggo:
     def speak(self, sound):
         return f"{self.name} says {sound}"
     
-    miles = Doggo("Miles", 4)
-    buddy = Doggo("Buddy", 9)
-    jack = Doggo("Jack", 3)
-    jim = Doggo("Jim", 5)
+miles = Doggo("Miles", 4)
+buddy = Doggo("Buddy", 9)
+jack = Doggo("Jack", 3)
+jim = Doggo("Jim", 5)
+
+# create inherited class  and its instance 
+
+class Labrador(Doggo):
+    pass
+
+gandhi = Labrador("Gandhi", 13)
+
+print(gandhi.name, gandhi.age)
+print(gandhi.species)
+print(gandhi)
+gandhi.speak("buf")
+print(isinstance(gandhi, Labrador))
+
+print(isinstance(gandhi, Doggo))
